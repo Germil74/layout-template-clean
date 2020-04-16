67f9ca7fdcbd1630459ea579fe51ccfd7b9b7494
Gennadiy <magnit_gena@mail.ru>
2020-04-16 07:36:35 +0400

    materialize commit 1

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,16 +1,16 @@
-(function($){
+/*(function($){
     $(function(){
 
         $('.sidenav').sidenav();
         $('.parallax').parallax();
 
     }); // end of document ready
-})(jQuery); // end of jQuery name space
+})(jQuery); */// end of jQuery name space
 
 
 window.onload = function() {
     svg4everybody({})
-   /* $('.sidenav').sidenav();
+  /*  $('.sidenav').sidenav();
     $('.parallax').parallax();*/
 }
 
